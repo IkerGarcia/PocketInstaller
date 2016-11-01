9ead14b37e718f21c57bd1bfefc6d37911df7373
Andrea Campanella <andrea@digiconsoles.com>
2016-11-01 15:08:31 +0000

    fixed typo in Pocket Home

--- Installers/PocketHome.sh
+++ Installers/PocketHome.sh
@@ -1,7 +1,7 @@
 echo "installing xinput-calibrator "
 sudo apt install -y xinput-calibrator > /dev/null
 echo "Setting up Pocket Home"
-wget -O install-pockethome http://bit.ly/29uWueR -C /tmp/
+wget install-pockethome http://bit.ly/29uWueR -O /tmp/
 sh /tmp/install-pockethome
 echo "Done"
 
