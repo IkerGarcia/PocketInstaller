be531e9c62098f364bb7d07e4ad1cf7e4b4e2572
IkerGarcia <ikergarcia0@gmail.com>
2016-11-09 00:03:51 +1100

    Update PocketInstaller.sh

--- PocketInstaller.sh
+++ PocketInstaller.sh
@@ -4,49 +4,49 @@ if hash zenity 2>/dev/null; then
   :
 else
   sudo apt-get update
-  sudo apt-get install -y zenity
+  sudo apt-get install zenity
 fi
 if hash yad 2>/dev/null; then
   :
 else 
   echo "deb http://pkg.bunsenlabs.org/debian bunsen-hydrogen  main" | sudo tee -a /etc/apt/sources.list
+  wget https://pkg.bunsenlabs.org/debian/pool/main/b/bunsen-keyring/bunsen-keyring_2016.7.2-1_all.deb
+  sudo dpkg -i bunsen-keyring_2016.7.2-1_all.deb
+  echo "#key added" | sudo tee -a /etc/apt/sources.list
   sudo apt-get update
-  sudo apt-get install --yes --force-yes yad
+  sudo apt-get install yad
+fi
+if grep -Fxq "deb http://pkg.bunsenlabs.org/debian bunsen-hydrogen  main" /etc/apt/sources.list && grep -Fxq "#key added" /etc/apt/sources.list; then
+  :
+else
+  wget https://pkg.bunsenlabs.org/debian/pool/main/b/bunsen-keyring/bunsen-keyring_2016.7.2-1_all.deb
+  sudo dpkg -i bunsen-keyring_2016.7.2-1_all.deb
+  sudo apt-get update
+  echo "#key added" | sudo tee -a /etc/apt/sources.list
 fi
 
 if hash mednafen 2>/dev/null; then
   :
 else
-  P1="Mednafen(GB,GBA,NES,SNES,NPC)|Installers/mednafen.sh"
+  P1="Mednafen(GB,GBA,NES,SNES,NPC)|/home/chip/PocketInstaller/Installers/mednafen.sh"
 fi
-
-if hash x64 2>/dev/null; then
+if hash vice 2>/dev/null; then
   :
 else
-  P2="Vice(C64,C128)|Installers/vice.sh"
+  P2="Vice(C64,C128)|/home/chip/PocketInstaller/Installers/vice.sh"
 fi
-
 if hash prboom  2>/dev/null; then
   :
 else
-  P3="Doom|Installers/doom.sh"
+  P3="Doom|/home/chip/PocketInstaller/Installers/doom.sh"
 fi
-
 if hash openttd 2>/dev/null; then
   :
 else 
-  P4="OpenTTD|Installers/openttd.sh"
+  P4="OpenTTD|/home/chip/PocketInstaller/Installers/openttd.sh"
 fi
-
 if hash dosbox 2>/dev/null; then
   :
-else 
-  P5="DOSBox|Installers/dosbox.sh"
-fi
-
-if hash scummvm 2>/dev/null; then
-  :
-else
   P6="ScummVM|Installers/scummvm.sh"
 fi
 P7="PocketHome|Installers/PocketHome.sh"
