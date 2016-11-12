05700bda3742ad38add517b5f2779d828812181f
IkerGarcia <ikergarcia0@gmail.com>
2016-11-12 17:13:58 +1100

    v2.3, see CHANGELOG

--- Installers/PocketHome.sh
+++
@@ -1,7 +0,0 @@
-echo "installing xinput-calibrator "
-sudo apt install -y xinput-calibrator > /dev/null
-echo "Setting up Pocket Home"
-wget install-pockethome http://bit.ly/29uWueR -O /tmp/
-sh /tmp/install-pockethome
-echo "Done"
-
