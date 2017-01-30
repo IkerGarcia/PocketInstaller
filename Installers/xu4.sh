6e0404c851a56e07f9b326113f92acf830f9f18e
Eric W. Brown <feneric@gmail.com>
2017-01-29 21:25:44 -0500

    Changed fetched datafile name to match expectations.

--- Installers/xu4.sh
+++ Installers/xu4.sh
@@ -5,7 +5,7 @@ svn checkout https://svn.code.sf.net/p/xu4/code/trunk /home/chip/xu4-code
 cp -p xu4.patch /home/chip/xu4-code/u4/src
 cd /home/chip/xu4-code/u4/src
 wget -O ultima4.zip http://www.ultima-universe.com/downloads/ultima4v101.zip
-wget -O u4upgrade.zip https://downloads.sourceforge.net/project/xu4/Ultima%204%20VGA%20Upgrade/1.3/u4upgrad.zip
+wget -O u4upgrad.zip https://downloads.sourceforge.net/project/xu4/Ultima%204%20VGA%20Upgrade/1.3/u4upgrad.zip
 patch -p0 < xu4.patch
 make
 strip coord dumpsavegame tlkconv u4 u4dec u4enc u4unpackexe
