edf2185d6496579d76cee87da8274214c5ff978e
Eric W. Brown <feneric@gmail.com>
2017-04-28 17:21:39 -0400

    Restored rogue to the list of BSD games.

--- Installers/bsd.sh
+++ Installers/bsd.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-sudo apt-get install -y bsdgames
+sudo apt-get install -y bsdgames bsdgames-nonfree
 
 sudo mkdir /usr/games/bsdgames
 
 cd /usr/games
 
-sudo mv -t /usr/games/bsdgames adventure battlestar gomoku hangman monop phantasia robots snake tetris-bsd hunt arithmetic atc backgammon bcd boggle caesar canfield cfscores cribbage dab go-fish hack mille morse number pig pom ppt primes quiz rain random rot13 sail snscore teachgammon trek wargames worms wtf wump worm countmail
+sudo mv -t /usr/games/bsdgames adventure battlestar gomoku hangman monop phantasia robots snake tetris-bsd hunt arithmetic atc backgammon bcd boggle caesar canfield cfscores cribbage dab go-fish hack mille morse number pig pom ppt primes quiz rain random rot13 sail snscore teachgammon trek wargames worms wtf wump worm countmail rogue
 
